0032915d14e835d6e2999b049cfefef6f514fd85
001ZD5744 <001ZD5744@LAPTOP-2D0MTLUE>
2021-06-11 14:52:55 +0530

    first commit

--- exception.java/src/com/example/app/App.java
+++ exception.java/src/com/example/app/App.java
@@ -6,53 +6,77 @@ import java.util.Scanner;
 
 import java.util.UUID;
 
+import com.example.Employee;
+import com.example.Manager;
 import com.example.Todo;
 
 public class App {
 
-	private static Scanner sc=new Scanner(System.in);
-	private Todo[] todos;
-	{
-		todos=new Todo[2];
-	}
-	public static void main(String args[])
-	{
-		App app=new App();
-		
-			for(int i=0; i<2;i++)
-			{
-				try {
-				String id=UUID.randomUUID().toString();
-				System.out.println("Enter the desc ");
-				String desc =sc.next();
-				System.out.println("Is Completed?");
-				boolean ic=sc.nextBoolean();
-				app.todos[i]=new Todo(id, desc, ic);
-			}
-				catch (ArrayIndexOutOfBoundsException e)
-				{
-						System.err.println("array index out of bound exception.");
-				}
-				catch(InputMismatchException im)
-				{
-					System.err.println("Enter the correct input");
-					i--;
-					continue;
-				}
-			}
-			for(int i=0;i<=5;i++)
-			{
-				try {
-					
-					System.out.println(app.todos[i]);
-					
-				} catch (ArrayIndexOutOfBoundsException e) {
-					System.err.println("Array Index Out Of Bound.");
-					
-				}
-				
-			}
-		}
+	private static Scanner scanner = new Scanner(System.in);
+
+	/*
+	 * private Todo[] todos;
+	 * 
+	 * { todos=new Todo[2]; }
+	 */
+
+	public static void main(String args[]) {
+		App app = new App();
+		/*
+		 * for(int i=0;i<2;i++) { try { System.out.print("ToDo Desc: "); String
+		 * tempDesc=scanner.next(); System.out.print("isCompleted? "); boolean
+		 * tempIsCompleted=scanner.nextBoolean(); app.todos[i]=new
+		 * Todo(UUID.randomUUID().toString(), tempDesc, tempIsCompleted);
+		 * 
+		 * 
+		 * } catch (ArrayIndexOutOfBoundsException e) {
+		 * System.err.println("array index out of bound exception."); } } for(int
+		 * i=0;i<=5;i++) { try {
+		 * 
+		 * System.out.println(app.todos[i]);
+		 * 
+		 * } catch (ArrayIndexOutOfBoundsException e) {
+		 * //System.out.println("Array Index Out Of Bound.");
+		 * 
+		 * }
+		 * 
+		 * }
+		 */
+		Employee employee = null;
+		Manager manager = new Manager();
+		app.check(manager);
+
+		app.check(employee);
 
 	}
+
+	private void check(Employee o) {
+		try {
+		//	int a = 100 / 0;
+			if (o instanceof Manager) {
+				Manager manager = (Manager) o;
+				System.out.println("Manager" + manager.toString());
+			} else {
+				System.outprintln(o.toString());
+			}
+
+		}
+
+		/*
+		 * catch (NullPointerException e) {
+		 * System.err.println("null pointer exception"); e.printStackTrace(); }
+		 */catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("array index out of bound exception");
+		}
+		catch (ArithmeticException e) {
+			System.err.println("arithemetic");
+			e.printStackTrace();
+		}
+
+		/*
+		 * catch (Exception e) { System.err.println(e.toString()); }
+		 */
+	}
+
+}
 
