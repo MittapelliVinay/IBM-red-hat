aa2e5dbed5488afd0d7f8c235333ca6449a88d95
001ZD5744 <001ZD5744@LAPTOP-2D0MTLUE>
2021-06-11 15:32:43 +0530

    secound commit

--- exception.java/src/com/example/app/App.java
+++ exception.java/src/com/example/app/App.java
@@ -1,9 +1,6 @@
 package com.example.app;
 
-import java.util.InputMismatchException;
-
 import java.util.Scanner;
-
 import java.util.UUID;
 
 import com.example.Employee;
@@ -22,6 +19,15 @@ public class App {
 
 	public static void main(String args[]) {
 		App app = new App();
+		try {
+			
+		} 
+		catch(Exception e) {
+			// TODO: handle finally clause
+		}
+		finally {
+			
+		}
 		/*
 		 * for(int i=0;i<2;i++) { try { System.out.print("ToDo Desc: "); String
 		 * tempDesc=scanner.next(); System.out.print("isCompleted? "); boolean
@@ -42,41 +48,42 @@ public class App {
 		 * 
 		 * }
 		 */
-		Employee employee = null;
-		Manager manager = new Manager();
-		app.check(manager);
+		/*
+		 * Employee employee = null; Manager manager = new Manager(); if(employee==null)
+		 * { throw new NullPointerException("Null Pointer Exception Occured"); }
+		 * app.check(manager);
+		 */
+		/*
+		 * try { app.check(employee);
+		 * 
+		 * } catch (NullPointerException e) { e.printStackTrace(); }
+		 */
+		
+		// app.check(employee);
 
-		app.check(employee);
-
+		app.div(100, 0);
 	}
 
-	private void check(Employee o) {
-		try {
+	private void check(Employee o)throws NullPointerException {
+	
 		//	int a = 100 / 0;
 			if (o instanceof Manager) {
 				Manager manager = (Manager) o;
 				System.out.println("Manager" + manager.toString());
 			} else {
-				System.outprintln(o.toString());
+				System.out.println(o.toString());
 			}
 
 		}
+	
+	private void div(int a,int b)throws ArithmeticException
+	{
+		System.out.println(a/b);
+	}
+	
+	
 
-		/*
-		 * catch (NullPointerException e) {
-		 * System.err.println("null pointer exception"); e.printStackTrace(); }
-		 */catch (ArrayIndexOutOfBoundsException e) {
-			System.err.println("array index out of bound exception");
-		}
-		catch (ArithmeticException e) {
-			System.err.println("arithemetic");
-			e.printStackTrace();
-		}
-
-		/*
-		 * catch (Exception e) { System.err.println(e.toString()); }
-		 */
+		
 	}
 
-}
 
