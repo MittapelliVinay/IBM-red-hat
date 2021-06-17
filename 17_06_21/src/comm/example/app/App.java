d0a32ac94a73ea6cd92e9faca70b22cfc52c0718
001ZD5744 <001ZD5744@LAPTOP-2D0MTLUE>
2021-06-17 10:18:34 +0530

    Threads

--- 17_06_21/src/comm/example/app/App.java
+++ 17_06_21/src/comm/example/app/App.java
@@ -1,10 +1,12 @@
 package comm.example.app;
-
+import comm.example.PrintThread;
 import comm.example.ThreadTest;
 public class App {
 	
 
 	public static void main(String[] args) {
+		
+		/*
 		ThreadTest test=new ThreadTest();
 
 		Thread t=new Thread(test);
@@ -13,7 +15,12 @@ public class App {
 		Thread t1=new Thread(test);
 		System.out.println(t1.getName());
 		t1.start();
+		*/
 		
+		PrintThread thread1=new PrintThread("A");
+		thread1.start();
+		PrintThread thread2=new PrintThread("B");
+		thread2.start();
+	}
 	}
 
-}
