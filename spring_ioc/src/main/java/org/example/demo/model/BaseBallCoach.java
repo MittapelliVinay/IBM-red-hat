4d8ca11a7d6fd5d32dffad4c3c9f2a1b4583e417
001ZD5744 <001ZD5744@LAPTOP-2D0MTLUE>
2021-06-24 12:41:56 +0530

    push1

--- spring_ioc/src/main/java/org/example/demo/model/BaseBallCoach.java
+++ spring_ioc/src/main/java/org/example/demo/model/BaseBallCoach.java
@@ -1,11 +1,2 @@
 package org.example.demo.model;
 
-public class BaseBallCoach implements Coach {
-
-	@Override
-	public String getDailyworkOut() {
-		// TODO Auto-generated method stub
-		return "practice back volley today";
-	}
-
-}
