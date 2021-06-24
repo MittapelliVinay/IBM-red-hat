26f2aa4d4d349411285dd78b793011ce75b65726
001ZD5744 <001ZD5744@LAPTOP-2D0MTLUE>
2021-06-24 16:44:14 +0530

    added basic DI

--- spring_ioc_demo/src/main/java/org/example/demo/model/FortuneService.java
+++ spring_ioc_demo/src/main/java/org/example/demo/model/FortuneService.java
@@ -1,8 +1,7 @@
 package org.example.demo.model;
 
-public interface Coach {
+public interface FortuneService {
 	
-	public String getDailyworkOut();
 	public String getDailyFortune();
 
 }
--- spring_ioc_demo/src/main/java/org/example/demo/model/SwimmingCoach.java
+++ spring_ioc_demo/src/main/java/org/example/demo/model/SwimmingCoach.java
@@ -2,10 +2,25 @@ package org.example.demo.model;
 
 public class SwimmingCoach implements Coach {
 
+	private FortuneService fortuneService;
+
+	// constructor injection
+
 	@Override
 	public String getDailyworkOut() {
 		// TODO Auto-generated method stub
 		return "practice back swimming today";
+	}
+
+	public SwimmingCoach(FortuneService fortuneService) {
+		super();
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getDailyFortune();
 	}
 
 }
