aa9c27b4ced4000037de6fe0571a80d26742d660
001ZD5744 <001ZD5744@LAPTOP-2D0MTLUE>
2021-06-25 17:57:14 +0530

    spring commit

---
+++ spring_ioc_javabased/src/main/java/org/example/demo/dao/AccountDao.java
@@ -0,0 +1,13 @@
+package org.example.demo.dao;
+
+import java.util.List;
+
+import org.example.demo.pojo.Account;
+
+public interface AccountDao {
+	
+	public Account createAccount(Account account);
+	
+	public List<Account> displayAllAvailableAccount();
+
+}
